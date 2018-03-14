f7daf1400a68220d4ab7a79cb0783491705e9eeb
paarthmadan <madanhockey@hotmail.com>
2018-03-14 12:14:34 -0400

    (add): download preliminary pdf and pipes res into file

--- cccme.js
+++ cccme.js
@@ -1,6 +1,16 @@
-var errors = require('./errors.js');
+const errors = require('./errors.js');
+const request = require('request');
+const http = require('http');
+const bl = require('bl');
+let fs = require('fs');
+let PDFParser = require('pdf2json');
+
+
 const args = process.argv;
+const base_url = "http://www.cemc.uwaterloo.ca/contests/computing/2015/stage%201/juniorEn.pdf";
 var size = 5;
+let pdfParser = new PDFParser();
+
 
 //ERROR HANDLING
 if(args.length != 6 || errors.checkData(args) != 200){
@@ -13,6 +23,7 @@ args[2] = args[2].toUpperCase();
 args[3] = args[3].toUpperCase();
 args[4] = parseInt(args[4]);
 args[5] = parseInt(args[5]);
+
 
 function randomizeProblems (args, callback){ 
 	var problem_sets = [];
@@ -37,6 +48,13 @@ function randomizeProblems (args, callback){
 function generated(problem_sets, years){
 	console.log(problem_sets);
 	console.log(years);
+	var file = fs.createWriteStream("./pdf/test.pdf");
+	http.get(base_url, function (response) {
+		response.pipe(file);
+		response.on("end", () => {
+			console.log("Done");
+		})
+     });
 }
 
 randomizeProblems(args, generated);
